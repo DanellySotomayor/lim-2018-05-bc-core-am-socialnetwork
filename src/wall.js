8fcd50725af7593aa5a8068610d8245d9ccbace7
ticze <ticze2598@gmail.com>
2018-07-17 15:33:47 -0500

    acomodando los cambios

--- src/wall.js
+++ src/wall.js
@@ -44,7 +44,7 @@ db.collection("users").onSnapshot((querySnapshot) => {
         <i class="fas fa-ellipsis-h"></i>
         </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-          <button class="dropdown-item btn-sm" type="button"  onclick="editar('${doc.id}','${doc.data().first}')"><i class="fas     fa-pen"></i>Editar</button>
+          <button class="dropdown-item btn-sm" type="button"  onclick="editar('${doc.id}','${doc.data().first}')"><i class="fas fa-pen"></i>Editar</button>
           <button class="dropdown-item btn-sm" type="button" onclick="eliminar('${doc.id}')"><i class="fas fa-trash-alt"></i>Eliminar</button>
         </div>
         <button type="button"><i class="fas fa-heart"></i> Like</button>
@@ -88,3 +88,5 @@ function editar(id, nombre) {
       });
   }
 }
+
+//contar like
