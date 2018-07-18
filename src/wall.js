ee9771311e66509bb52f56dd6e45ddedc2e7b48b
MalenaZP <malenazp@gmail.com>
2018-07-18 08:09:52 -0500

    perfil design

--- src/wall.js
+++ src/wall.js
@@ -38,10 +38,10 @@ db.collection("users").onSnapshot((querySnapshot) => {
     console.log(`${doc.id} => ${doc.data().first}`);
     tabla.innerHTML += `
       <div> 
-        <textarea disabled>${doc.data().first}</textarea>
+      <br><br>
+        <p class="font-weight-bold lead caja-post">${doc.data().first}</p>
         <button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true"
         aria-expanded="false">
-        <i class="fas fa-ellipsis-h"></i>
         </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
           <button class="dropdown-item btn-sm" type="button"  onclick="editar('${doc.id}','${doc.data().first}')"><i class="fas     fa-pen"></i>Editar</button>
